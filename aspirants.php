ba16a22df52d37f119e51acdbea384efabb57ba9
itcyborg <itcyborg@outlook.com>
2017-03-27 12:33:18 +0300

    Updates Created payments 1. you can now enter payment transactions made for approval 2. Confirm and approve payments

--- aspirants.php
+++ aspirants.php
@@ -1,155 +1,239 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: itcyb
- * Date: 3/3/2017
- * Time: 9:12 AM
- */
-session_start();
-include "system/conn.php";
-$sql = "SELECT aspirants.*,profile.* FROM aspirants JOIN profile ON profile.UserID=aspirants.UserID WHERE Status='1'";
-$result = $conn->query($sql) or die($conn->error);
-$output = "";
-while ($row = $result->fetch_assoc()) {
-    $output .= "<tr>
-<td>" . $row['FiirstName'] . " " . $row['MiddleName'] . " " . $row['LastName'] . "</td>
-<td>" . $row['Position'] . "</td>
-<td>" . $row['Location'] . "</td>
-<td><a href='views/?action=view&category=aspirants&id=" . $row['UserID'] . "'>View</a></td>
-</tr>";
-}
-?>
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
 <head>
-    <meta charset="utf-8"/>
-    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png"/>
-    <link rel="icon" type="image/png" href="../../assets/img/favicon.png"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msvalidate.01" content="3EA6FC87BA4A8513C359EA77AD427210"/>
+    <title>Safemoon</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+          content="Get to know, converse and talk to your political aspirant. Know who is vying for which position at your place"/>
+    <meta name="keywords" content="Safemoon,politics,kenya politics,political aspirants,aspirants"/>
+    <meta name="author" content="Itcyborg Designs, http://itcyborg.webuda.com"/>
 
-    <title>Safemoon - Aspirants</title>
+    <!--
+      //////////////////////////////////////////////////////
 
-    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
-    <meta name="viewport" content="width=device-width"/>
+      FREE HTML5 TEMPLATE
+      DESIGNED & DEVELOPED by FREEHTML5.CO
 
-    <!-- Bootstrap core CSS     -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+      Website: 		http://freehtml5.co/
+      Email: 			info@freehtml5.co
+      Twitter: 		http://twitter.com/fh5co
+      Facebook: 		https://www.facebook.com/fh5co
 
-    <!--  Material Dashboard CSS    -->
-    <link href="assets/css/material-dashboard.css" rel="stylesheet"/>
+      //////////////////////////////////////////////////////
+       -->
 
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="assets/css/demo.css" rel="stylesheet"/>
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content=""/>
+    <meta name="twitter:image" content=""/>
+    <meta name="twitter:url" content=""/>
+    <meta name="twitter:card" content=""/>
 
-    <!--     Fonts and icons     -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic,700italic' rel='stylesheet'
           type='text/css'>
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <!-- Simple Line Icons -->
+    <link rel="stylesheet" href="assets/css/simple-line-icons.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <!-- Owl Carousel  -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <!-- Style -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <!-- Modernizr JS -->
+    <script src="assets/js/modernizr-2.6.2.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-
 <body>
-
-<div class="wrapper">
-    <div class="main-panel">
-        <nav class="navbar navbar-transparent navbar-absolute">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">Safemoon</a>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="signup.php">
-                                <span>Sign Up</span>
-                                <i class="material-icons">person add</i>
-                                <p class="hidden-lg hidden-md">Sign Up</p>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="login.php">
-                                <span>Login</span>
-                                <i class="material-icons">input</i>
-                                <p class="hidden-lg hidden-md">Login</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
+<header role="banner" id="fh5co-header">
+    <div class="fluid-container">
+        <nav class="navbar navbar-default">
+            <div class="navbar-header">
+                <!-- Mobile Toggle Menu Button -->
+                <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle" data-toggle="collapse" data-target="#navbar"
+                   aria-expanded="false" aria-controls="navbar"><i></i></a>
+                <a class="navbar-brand" href="login.html"><span>Safe</span>moon</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="signup.php">Sign up</a></li>
+                </ul>
             </div>
         </nav>
+    </div>
+</header>
 
-        <div class="content">
-            <div class="container-fluid">
-                <div class="row-fluid col-md-12 col-lg-12 col-sm-12">
-                    <div class="card">
-                        <div class="card-header card-profile">Aspirants</div>
-                        <div class="card-content">
-                            <table class="table table-responsive table-full-width" id="aprirants">
-                                <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Location</th>
-                                    <th>Details</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <?php echo $output; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+<section id="fh5co-services" data-section="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 to-animate">
+                <h3>Know your Future Leaders</h3>
+                <p>Get to know who is vying for a position at your ward, constituency, county and even
+                    presidency.</p>
+            </div>
+            <div class="col-md-6 to-animate-2">
+                <div class="call-to-action text-right">
+                    <a href="aspirants.php" class="sign-up">More</a>
                 </div>
             </div>
         </div>
+    </div>
+</section>
 
-        <footer class="footer">
-            <div class="container-fluid">
-                <nav class="pull-left">
-                    <ul>
-                        <li>
-                            <a href="index.html">
-                                Home
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <p class="copyright pull-right">
-                    &copy;
-                    <script>document.write(new Date().getFullYear())</script>
-                    <a href="http://www.itcyborg.webuda.com">Powered by Itcyborg Designs </a>
-                    <small>Theme by <a href="http://www.creative-tim.com">Creative Tim</a></small>
-                </p>
+<div class="getting-started getting-started-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 to-animate">
+                <h3>Getting Started</h3>
+                <p>Sign up for a free account and talk to your future leaders, either at county, constituency or
+                    ward level.</p>
             </div>
-        </footer>
+            <div class="col-md-6 to-animate-2">
+                <div class="call-to-action text-right">
+                    <a href="signup.php" class="sign-up">Sign Up For Free</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
+<!--section id="fh5co-pricing" data-section="pricing">
+
+</section>
+
+<section id="fh5co-services" data-section="services">
+
+</section>
+
+<section id="fh5co-team" data-section="team">
+
+</section>
+
+<section id="fh5co-faq" data-section="faq">
+
+</section>
+
+<hr>
+
+<section id="fh5co-trusted" data-section="trusted">
+
+</section>
+
+<div class="getting-started getting-started-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 to-animate">
+                <h3>Getting Started</h3>
+                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. </p>
+            </div>
+            <div class="col-md-6 to-animate-2">
+                <div class="call-to-action text-right">
+                    <a href="#" class="sign-up">Sign Up For Free</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div-->
+
+<div id="fh5co-footer" role="contentinfo">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 to-animate">
+                <h3 class="section-title">About Us</h3>
+                <p>We provide detailed information on your future leaders and leaders to be. We give you a platform
+                    to interact with future and current leaders.</p>
+                <p class="copy-right">&copy; 2017 Safemoon <br>All Rights Reserved. <br>
+                    Designed by <a href="http://itcyborg.webuda.com/" target="_blank">Itcyborg Designs<br>
+                        Credits by <a href="http://freehtml5.co/" target="_blank">FREEHTMML5</a>
+                </p>
+            </div>
+
+            <div class="col-md-4 to-animate">
+                <h3 class="section-title">Our Address</h3>
+                <ul class="contact-info">
+                    <li><i class="icon-phone"></i>+254 722 235 880</li>
+                    <li><i class="icon-envelope"></i><a href="mailto:admin@safemoon.com">admin@safemoon.com</a></li>
+                </ul>
+                <h3 class="section-title">Connect with Us</h3>
+                <ul class="social-media">
+                    <li><a href="https://www.facebook.com/safemoon" class="facebook" target="_blank"><i
+                                    class="icon-facebook"></i></a></li>
+                    <li><a href="https://www.twitter.com/safemoonke" target="_blank" class="twitter"><i
+                                    class="icon-twitter"></i></a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 to-animate">
+                <h3 class="section-title">Drop us a line</h3>
+                <form class="contact-form">
+                    <div class="form-group">
+                        <label for="name" class="sr-only">Name</label>
+                        <input type="name" class="form-control" id="name" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <label for="email" class="sr-only">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="message" class="sr-only">Message</label>
+                        <textarea class="form-control" id="message" rows="7" placeholder="Message"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" id="btn-submit" class="btn btn-send-message btn-md" value="Send Message">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- jQuery -->
+<script src="assets/js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="assets/js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="assets/js/jquery.waypoints.min.js"></script>
+<!-- Stellar Parallax -->
+<script src="assets/js/jquery.stellar.min.js"></script>
+<!-- Owl Carousel -->
+<script src="assets/js/owl.carousel.min.js"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+<script src="assets/js/google_map.js"></script>
+<!-- Main JS (Do not remove) -->
+<script src="assets/js/main.js"></script>
+
 </body>
-
-<!--   Core JS Files   -->
-<script src="assets/js/jquery-3.1.0.min.js" type="text/javascript"></script>
-<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="assets/js/material.min.js" type="text/javascript"></script>
-
-<!--  Charts Plugin -->
-<script src="assets/js/chartist.min.js"></script>
-
-<!--  Notifications Plugin    -->
-<script src="assets/js/bootstrap-notify.js"></script>
-
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
-
-<!-- Material Dashboard javascript methods -->
-<script src="assets/js/material-dashboard.js"></script>
-
-<!-- Material Dashboard DEMO methods, don't include it in your project! -->
-<script src="assets/js/demo.js"></script>
-
 </html>
+
