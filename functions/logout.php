bccff0f02d027779022e5db995167d06ed99f52b
itcyborg <itcyborg@outlook.com>
2017-03-29 17:05:24 +0300

    Updates Created payments 1. you can now enter payment transactions made for approval 2. Confirm and approve payments

--- functions/logout.php
+++ functions/logout.php
@@ -5,7 +5,9 @@
  * Date: 3/23/2017
  * Time: 12:16 PM
  */
-
+session_start();
+unset($_SESSION['role']);
+unset($_SESSION['userid']);
 session_unset();
 session_destroy();
 header('location:../login.php');
